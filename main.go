87a06da3da0a8a8f8022b88eb3348b83a90585cd
Nusen-ai <inininaq1977@gmail.com>
2025-11-17 20:00:36 +0800

    optimize

--- main.go
+++ main.go
@@ -1,26 +1,24 @@
 package main
 
-import (
-	advance "nus_go_jo/task2"
-)
+import "nus_go_jo/task2"
 
 func main() {
-	// basis.Job1TestDemo()
-	// basis.Job2TestPalindromeString()
-	// basis.Job3Test()
-	// basis.Job4Test()
-	// basis.Job5Test()
-	// basis.Job6Test()
-	// basis.Job7Test()
-	// basis.Job8Test()
-	// advance.Job1TestDemo()
-	// advance.Job2TestDemo()
-	// advance.Job3TestDemo()
-	// advance.Job4TestDemo()
-	// advance.Job5TestDemo()
-	// advance.Job6TestDemo()
-	// advance.Job7TestDemo()
-	// advance.Job8TestDemo()
-	// advance.Job9TestDemo()
-	advance.Job10TestDemo()
+	// task1.Job1TestDemo()
+	// task1.Job2TestPalindromeString()
+	// task1.Job3Test()
+	// task1.Job4Test()
+	// task1.Job5Test()
+	// task1.Job6Test()
+	// task1.Job7Test()
+	// task1.Job8Test()
+	// task2.Job1TestDemo()
+	// task2.Job2TestDemo()
+	// task2.Job3TestDemo()
+	// task2.Job4TestDemo()
+	// task2.Job5TestDemo()
+	// task2.Job6TestDemo()
+	// task2.Job7TestDemo()
+	// task2.Job8TestDemo()
+	// task2.Job9TestDemo()
+	task2.Job10TestDemo()
 }
